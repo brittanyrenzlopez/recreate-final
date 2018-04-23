e2e1a4f442b3f6a9b2fc1127e391959796abc09d
Brittany Renz-Lopez <brittanyrenzlopez@gmail.com>
2018-04-23 14:37:03 -0700

    fix client

--- server.js
+++ server.js
@@ -28,15 +28,6 @@ require('./config/passport')(passport);
 // use routes
 app.use('/api/users', users);
 
-// serve static assets in prod
-if (process.env.NODE_ENV === 'production') {
-  // set static folder
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server's good to go on port ${port}`));
